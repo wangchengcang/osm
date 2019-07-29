1d2e03eef82982547e2f5db747787abe8bf07ec8
guohaoyu6378 <1378394269@qq.com>
2019-07-29 15:40:57 +0800

    963.

--- sm/routes/users.js
+++ sm/routes/users.js
@@ -6,5 +6,53 @@ var url=require('url');
 var querystring=require('querystring');
 var path=require('path');
 var router = express.Router();
+// 注册
+router.post('/login',function(req,res){
+	res.setHeader('Access-Control-Allow-Origin','*')
+	var json=req.body;
+	sql.con({
+		arr:[json.user],
+		sql:'select * from login where user=?',
+		success(data){
+			if(data.length){
+				res.send('no')
+			}else{
+				sql.con({
+					arr:[json.name,json.user,json.pass,json.img],
+					sql:'insert into usera(name,user,pass,img) values(?,?,?,?)',
+					success(data){
+						res.send('ok')
+					},
+					error(err){
+						res.send(err)
+					}
+				})
+			}
+		}
+	})
+})
+//登录 
+router.post('/logon',function(req,res){
+	res.setHeader('Access-Control-Allow-Origin','*')
+	var json=req.body;
+	sql.con({
+		arr:[json.user,json.pass],
+		sql:'select * from login where user=? and pass=?',
+		success(data){
+			if(data.length){
+     		    data[0].pass='';
+     		    res.send(data[0])
+			}else{
+				res.send('ok')
+			}
+		},
+		error(err){
+			res.send(err)
+		}
+		
+	})
+})
+
+
 
 module.exports = router;
